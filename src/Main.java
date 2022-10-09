b315bab8135e5de1fb5f180fef99796491e88f53
Thomas <Thomas@Tannenberg.online>
2022-10-09 18:14:29 +0200

    start coding

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+package online.Tannenberg;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Implement a program that asks the user for numbers (the program first prints "Write numbers: ")
+        // until the user gives the number -1. When the user writes -1, the program prints "Thx! Bye!" and ends.
+
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
