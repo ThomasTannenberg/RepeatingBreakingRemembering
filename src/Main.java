5b6e5b1fdb09b4acdd5d6af5186482293c77d731
Thomas <Thomas@Tannenberg.online>
2022-10-09 20:03:11 +0200

    Part 2 completed

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        //Implement a program that asks the user for numbers (the program first prints "Write numbers: ")
-        // until the user gives the number -1. When the user writes -1, the program prints "Thx! Bye!" and ends.
+        //Implement a program that asks the user for numbers (the program first prints "Write numbers:")
+        //until the user gives the number -1. When the user writes -1, the program prints "Thx! Bye!" and ends.
+        //Extend the program so that it prints the sum of the numbers (not including the -1) the user has written.
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Write numbers: ");
-        int number = 0;
+        System.out.println("Write numbers:");
+        int input = 0;
+        int numbers = 0;
+        int sum = 0;
 
-        while (number > -1 || number < -1) {
-            number = Integer.valueOf(scanner.nextLine());
+        while (input > -1 || input < -1) {
+            input = Integer.valueOf(scanner.nextLine());
+            numbers++;
+            sum += input;
         }
-        if (number == -1) {
+        if (input == -1) {
             System.out.println("Thx! Bye!");
+
         }
+        System.out.println("sum:" + (sum +1));
     }
 }
